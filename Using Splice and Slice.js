a6177d19da7783a3c41a976adcab964af5184bc0
liochonfu <liochonfu@gmail.com>
2023-01-05 01:28:15 +0800

    first commit

---
+++ Using Splice and Slice.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const hollywoodDirectors = [
+  'Martin Scorsese',
+  'David Dhawan',
+  'Steven Spielberg',
+  'Christopher Nolan',
+  'Quentin Tarantino',
+  'James Cameron',
+];
+
+const removeAndInsert = (arr, elem, newElem) => {
+    const findElem = arr.findIndex((e) => e === elem);
+    const tempArray = [...arr];
+    tempArray.splice(findElem, 1, newElem);
+    return tempArray;
+};
+const topThree = (arr) => arr.slice(0, 3);
+
+const revisedList = removeAndInsert(hollywoodDirectors, 'David Dhawan', 'David Lean');
+const directors = topThree(revisedList);
+console.log(`Hollywood Masterclass featuring ${directors}`);
